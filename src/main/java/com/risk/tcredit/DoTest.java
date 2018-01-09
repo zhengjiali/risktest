b8e7211438e41433208c672b3893e81d46183f48
zhengjiali <zhengjiali@zhengjialideMacBook-Air.local>
2018-01-09 22:50:04 +0800

    add properties

---
+++ src/main/java/com/risk/tcredit/DoTest.java
@@ -0,0 +1,79 @@
+package com.risk.tcredit;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.*;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream; 
+import java.util.Iterator;
+import java.util.Properties;
+import io.restassured.response.Response;
+public class DoTest {	
+
+	String risk_user;
+	
+	@BeforeTest
+	public void beforeTest(){
+		
+		Properties prop = new Properties();     
+        try{
+            //读取属性文件a.properties
+            InputStream in = new BufferedInputStream (new FileInputStream("config.properties"));
+            prop.load(in);     ///加载属性列表
+            risk_user = prop.getProperty("risk_user");
+            baseURI = prop.getProperty("baseURI");
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+    } 
+
+	
+	@Test(enabled=false)
+	public void createParam(){
+		
+		Response createRes = 
+				given()
+					.cookie("_risk_user",risk_user)
+					.formParam("funCode", 302)
+					.formParam("nameEn", "test122102")
+					.formParam("nameCn", "test122102")
+					.formParam("dataType", 1)
+					.formParam("productLine[]", 64,65)
+				.when()
+					.post("/riskData/trics/param/addAttr")
+				.then()
+					.statusCode(200)
+					.body("status", equalTo(0))
+				.extract()
+					.response();
+	}
+	
+	@Test
+	public void doTest(){
+		String str1="[{\"id\":\"7770\",\"nameEn\":\"AutoApi1\",\"dataType\":\"1\",\"value\":\"1\",\"variableType\":\"1\"},{\"id\":\"7985\",\"nameEn\":\"AutoParam50\",\"dataType\":\"1\",\"value\":\"-1\",\"variableType\":\"1\"}]";
+		String str2="[{\"id\":\"2518\",\"nameEn\":\"switchswww3333\",\"dataType\":\"4\",\"expected\":\"\"}]";
+
+				given()
+					.log().all()
+					.contentType("application/x-www-form-urlencoded; charset=UTF-8")
+					.cookie("_risk_user",risk_user)
+					.formParam("testType",1)
+					.formParam("moduleId",3814)
+					.formParam("variablesIn",str1)
+					.formParam("variablesIn",str2)
+				.when()
+					.post("/risk/trics/rule/doTest")
+				.then()
+					.statusCode(200)
+					.body("status", equalTo(0))
+					.assertThat().body(matchesJsonSchemaInClasspath("doTest.json"));
+
+	}
+
+}
--- src/main/java/com/risk/tcredit/LoginTest.java
+++ src/main/java/com/risk/tcredit/LoginTest.java
@@ -2,38 +2,56 @@ package com.risk.tcredit;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.restassured.response.Response;
 import static io.restassured.module.jsv.JsonSchemaValidator.*;
-
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream; 
+import java.util.Iterator;
+import java.util.Properties; 
 
 public class LoginTest {
 	
-	public String baseURI;
-	
-	@BeforeTest
-	@Parameters("baseUrl")
-	public void beforeTest(String baseUrl){
-		baseURI = baseUrl;
-	}
+	String risk_user;
 	
 	@Test
-	@Parameters({"username","password"})
-	public void loginSucces(String username,String password){
+	@Parameters({"baseUrl","username","password"})
+	public void loginSucces(String baseUrl,String username,String password){
+		baseURI=baseUrl;
+		Response resLogin=
 		given()
 			.log().all()
 			.contentType("application/x-www-form-urlencoded; charset=UTF-8")
 			.formParam("userName",username)
 			.formParam("password",password)
 		.when()
-			.post(baseURI+"/engine/trics/doLogin")
-		.then()
-			.statusCode(200)
-			.body("status", equalTo(0))
-			.assertThat().body(matchesJsonSchemaInClasspath("login.json"));
+			.post("/risk/trics/doLogin");
+		resLogin.then()
+				.statusCode(200)
+				.body("status", equalTo(0))
+				.assertThat().body(matchesJsonSchemaInClasspath("login.json"));
+		risk_user = resLogin.getCookie("_risk_user");
+	}
+	
+	@AfterTest
+	public void afterTest(){
+		Properties prop = new Properties();
+		try{
+			FileOutputStream oFile = new FileOutputStream("config.properties", true);//true表示追加打开
+            prop.setProperty("risk_user", risk_user);
+            prop.setProperty("baseURI", baseURI);
+            prop.store(oFile, "The New properties file");
+            oFile.close();
+		}
+		catch(Exception e){
+            System.out.println(e);
+        }
 	}
 	
 	
