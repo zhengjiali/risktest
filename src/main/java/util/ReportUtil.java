e368d5cc4c3f97167aa5ead210018adecc72fc9d
zhengjiali <zhengjiali2014@163.com>
2018-01-16 15:21:30 +0800

    修改报告样式

---
+++ src/main/java/util/ReportUtil.java
@@ -0,0 +1,31 @@
+package util;
+
+import org.testng.Reporter;
+
+import com.aventstack.extentreports.utils.StringUtil;
+
+import java.util.Calendar;
+
+public class ReportUtil {
+	private static String reportName = "自动化测试报告";
+
+	private static String splitTimeAndMsg = "===";
+	public static void log(String msg) {
+		long timeMillis = Calendar.getInstance().getTimeInMillis();
+		Reporter.log(timeMillis + splitTimeAndMsg + msg, true);
+	}
+
+	public static String getReportName() {
+		return reportName;
+	}
+
+	public static String getSpiltTimeAndMsg() {
+		return splitTimeAndMsg;
+	}
+
+	public static void setReportName(String reportName) {
+		if(StringUtil.isNotNullOrEmpty(reportName)){
+			ReportUtil.reportName = reportName;
+		}
+	}
+}
